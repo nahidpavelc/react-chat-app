cfac5f07a69088797ece7e4525df359e47268235
nahidpavelc <nahidpavelchowdhury777@hotmail.com>
2022-01-26 20:40:16 +0600

    new update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
     <ChatEngine
       height="100vh"
       projectID="6afd34d3-73dc-4c4c-b02a-4303c9f50023"
-      userName="nahid"
-      userSecret="Nahid777"
+      userName="Shorif"
+      userSecret="12345"
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,10 +1,34 @@
+import { EyeInvisibleFilled } from '@ant-design/icons';
 import React from 'react';
 
-const TheirMessage = () => {
+const TheirMessage = ({ lastMessage, message }) => {
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.user !== message.sender.username;
+
     return (
-        <div>
-            <h1>Their Message</h1>
-        </div>
+        <div className='message-row'>
+            {isFirstMessageByUser && (
+                <div
+                    className='message-avatar'
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+
+            {message?.attachment?.length > 0
+                ? (
+                    <img
+                        src={message.attachment[0].file}
+                        alt="message-attachment"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className='message' style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
+
+        </div >
     );
 };
 
